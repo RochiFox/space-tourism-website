fec91dd0f962738366bf1b66b564b0209041e0c9
Yevhenii Vyshnevskyi <rochithefox@gmail.com>
2023-11-24 14:28:53 +0200

    Added: added navigation styles

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,24 +1,73 @@
 import * as React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
 
 const Navigation = () => {
+  const [activeLink, setActiveLink] = useState(window.location.pathname);
+
+  const handleLinkClick = (path: string) => {
+    setActiveLink(path);
+  };
+
   return (
     <nav className="navigation">
-      <ul className="navigation__links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/destination">Destination</Link>
-        </li>
-        <li>
-          <Link to="/crew">Crew</Link>
-        </li>
-        <li>
-          <Link to="/technology">Technology</Link>
-        </li>
-      </ul>
+      <Link
+        to="/"
+        className="navigation__logo-link"
+        onClick={() => handleLinkClick("/")}
+      >
+        <button className="navigation__logo-btn" />
+      </Link>
+      <div className="navigation__block">
+        <hr className="navigation__line" />
+        <ul className="navigation__links">
+          <li>
+            <Link
+              to="/"
+              className={`navigation__link ${
+                activeLink === "/" ? "active" : ""
+              }`}
+              onClick={() => handleLinkClick("/")}
+            >
+              <span className="navigation__bold-link">00</span> Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/destination"
+              className={`navigation__link ${
+                activeLink === "/destination" ? "active" : ""
+              }`}
+              onClick={() => handleLinkClick("/destination")}
+            >
+              <span className="navigation__bold-link">01</span> Destination
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/crew"
+              className={`navigation__link ${
+                activeLink === "/crew" ? "active" : ""
+              }`}
+              onClick={() => handleLinkClick("/crew")}
+            >
+              <span className="navigation__bold-link">02</span> Crew
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/technology"
+              className={`navigation__link ${
+                activeLink === "/technology" ? "active" : ""
+              }`}
+              onClick={() => handleLinkClick("/technology")}
+            >
+              <span className="navigation__bold-link">03</span> Technology
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
