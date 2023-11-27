fe30a04e2f923a2d8964856b66c28f11af675695
Yevhenii Vyshnevskyi <rochithefox@gmail.com>
2023-11-27 08:45:57 +0200

    Added: added home section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,19 @@ import "./index.scss";
 const Home = () => {
   return (
     <div className="home home_background-img">
-      <div className="home__info"></div>
+      <div className="home__info">
+        <div className="home__text">
+          <p className="home__pretitle">So, you want to travel to</p>
+          <h1 className="home__title">SPACE</h1>
+          <p className="home__subtitle">
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <button className="home__explore-btn">Explore</button>
+      </div>
     </div>
   );
 };
